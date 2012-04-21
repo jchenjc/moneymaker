0181d245e09ab4a294b2bf708e7c1008d6525af7
Jian Chen <jchenping@gmail.com>
2012-04-21 03:57:59 -0700

    first commit, brand new app

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,22 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def google
+  end
+
+  def nintendo
+  end
+
+  def zynga
+  end
+
+  def microsoft
+  end
+
+  def yahoo
+  end
+
+  def intel
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moneymaker::Application.config.secret_token = 'c0a488649eb8adeec2492827533376829110b5dd47068ed171430a8393718325af709ab6101042d441962641011491527965098d5b7d952d820581dc26e3507f'
